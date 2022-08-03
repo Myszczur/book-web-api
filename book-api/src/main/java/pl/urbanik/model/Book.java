e654103681a900ee40ba656fe1a220a370233c19
Kamil <Kamil.Urbanik@o2.pl>
2022-08-03 21:21:48 +0200

    #0001 - add initialize class

---
+++ book-api/src/main/java/pl/urbanik/model/Book.java
@@ -0,0 +1,26 @@
+package pl.urbanik.model;
+
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+}
