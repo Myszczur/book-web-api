e8cbbcba0c3a2faf5b32f69a942325b617f8a2b1
Kamil <Kamil.Urbanik@o2.pl>
2022-08-04 10:45:09 +0200

    #0004 - add model & repository

---
+++ book/src/main/java/pl/urbanik/model/Book.java
@@ -0,0 +1,28 @@
+package pl.urbanik.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+}
